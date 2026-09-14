92652d6baf4ff20b6969d1cb938a3475f02df1d3
divyasuri348 <128ddsuri@frhsd.com>
2026-09-14 10:59:46 -0400

    Adding reservations

--- src/Main.java
+++ src/Main.java
@@ -5,11 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = sc.nextLine();
-        System.out.println("Did you make a reservation ");
+        System.out.println("Did you make a reservation: (Yes/No)");
         System.out.println("What do you want to order from the menu?");
         String order = sc.nextLine();
 
-
+        System.out.println("Did you make a reservation? (Yes/No)");
+        String reservation = sc.nextLine();
+        if (reservation.equals("Yes")) {
+            System.out.println("Enter");
+        }
 
     }
 }
