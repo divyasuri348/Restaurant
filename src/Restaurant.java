84244fb3184ba58a3139c5543ecbb61e78ccb686
divyasuri348 <128ddsuri@frhsd.com>
2026-09-14 10:30:39 -0400

    Restaurant class

--- src/Restaurant.java
+++ src/Restaurant.java
@@ -1,5 +1,12 @@
 import java.util.ArrayList;
 
 public class Restaurant {
-    ArrayList<Restaurant> menu;
+    String [] menu;
+    public Restaurant() {
+        menu = new String[10];
+    }
+    public void setMenu(){
+        menu[0] = "Hamburger";
+
+    }
 }
