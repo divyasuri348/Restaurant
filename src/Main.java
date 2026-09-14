b38e3854ba30d6f368854bdc20865d4dc45399bd
divyasuri348 <128ddsuri@frhsd.com>
2026-09-14 10:56:59 -0400

    Menu changes

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,11 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter your name");
         String name = sc.nextLine();
+        System.out.println("Did you make a reservation ");
         System.out.println("What do you want to order from the menu?");
         String order = sc.nextLine();
+
+
 
     }
 }
